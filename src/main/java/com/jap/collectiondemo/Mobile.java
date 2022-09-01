1e1b1800fd2c3a183a155d97a91b45afa7b1ab72
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-09-01 20:38:23 +0530

    Declared attriutes and getter and setter for them.

--- src/main/java/com/jap/collectiondemo/Mobile.java
+++ src/main/java/com/jap/collectiondemo/Mobile.java
@@ -12,7 +12,65 @@ public class Mobile {
 
    //create getter and setter for all the above attributes
 
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(String screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getBatteryLife() {
+        return batteryLife;
+    }
+
+    public void setBatteryLife(String batteryLife) {
+        this.batteryLife = batteryLife;
+    }
+
+    public String getStorageSpace() {
+        return storageSpace;
+    }
+
+    public void setStorageSpace(String storageSpace) {
+        this.storageSpace = storageSpace;
+    }
+
+    public int getCameraPixels() {
+        return cameraPixels;
+    }
+
+    public void setCameraPixels(int cameraPixels) {
+        this.cameraPixels = cameraPixels;
+    }
+
+
     //create parameterized constructor
 
-    //override toString() method
+
+
 }
